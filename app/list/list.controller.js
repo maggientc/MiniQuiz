d378f0c16e69c554d772b52d4d23d2cb1e466805
Maggie Ng <iamtaiching@gmail.com>
2016-06-12 13:58:03 -0700

    added base study help by hiding the nubmers

--- app/list/list.controller.js
+++ app/list/list.controller.js
@@ -9,7 +9,15 @@
     function ListController($log, ProductService) {
         var list = this;
 
+        list.showNumber = true;
+        list.showNumberLabel = 'Hide Nubmer';
         list.imgUrl = ProductService.imgUrl;
         list.data = ProductService.getAll();
+        list.displayNumber = displayNumber;
+
+        function displayNumber(){
+            list.showNumber = !list.showNumber;
+            list.showNumberLabel = list.showNumber? 'Hide Nubmer':'Show Nubmer';
+        }
     }
 })();
