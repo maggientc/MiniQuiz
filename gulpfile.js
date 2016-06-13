7b403cfe9dedaed008c362d4d81cbf56aafec051
Maggie Ng <iamtaiching@gmail.com>
2016-06-13 13:02:43 -0700

    added nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
     jshint = require('gulp-jshint'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    nodemon = require('gulp-nodemon');
 
 // Styles Task
 // 
 gulp.task('styles', function() {
-    gulp.src('assets/scss/**/*.scss') //find all scss file in scss directory
+    gulp.src('public/assets/scss/**/*.scss') //find all scss file in scss directory
         .pipe(sass({
             outputStyle: 'compact' //[compressed, expanded, compact]
         }))
-        .pipe(gulp.dest('assets/css/'));
+        .pipe(gulp.dest('public/assets/css/'));
 });
 
 
 gulp.task('lint', function() {
-    return gulp.src('app/**/*.js')
+    return gulp.src('public/app/**/*.js')
         .pipe(jshint({ esversion: 6 }))
         .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('server', function() {
+    nodemon({
+            script: 'server.js',
+            ext: 'html js',
+            ignore: [
+                'public/',
+                'node_modules/'
+            ],
+        })
+        .on('restart', function() {
+            console.log('restarted!')
+        })
 });
 
 /*gulp.task('connect', function() {
@@ -29,11 +44,11 @@ gulp.task('lint', function() {
 // Watch Task - look for changes
 // Watches JS
 gulp.task('watch', function() {
-    gulp.watch('assets/scss/**/*.scss', ['styles']);
-    gulp.watch('app/**/*.js', ['lint']);
+    gulp.watch('public/assets/scss/**/*.scss', ['styles']);
+    gulp.watch('public/app/**/*.js', ['lint']);
 });
 
 // can be a callback function when gulp run
 // gulp.task('default', function() {}); 
 
-gulp.task('default', ['lint', /*'connect',*/ 'styles', 'watch']);
+gulp.task('default', ['server', 'lint', /*'connect',*/ 'styles', 'watch']);
